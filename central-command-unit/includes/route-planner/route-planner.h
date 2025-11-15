501dd2cdc4fdcf36f3164e56fd9d327c8b3568d2
LukasG62 <lukas.grando.62@gmail.com>
2025-11-16 00:23:36 +0100

    feat: Ajout fichier du route-planner

---
+++ central-command-unit/includes/route-planner/route-planner.h
@@ -0,0 +1,17 @@
+/**
+ * @file route-planner.h
+ * @brief fichier d'en-tête pour le service route planner
+ * @details
+ * Définit les structures et fonctions spécifiques au service route planner. ainsi que la version du service.
+ */
+
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
+
+#include "core/core.h"
+#include "route-planner/route_planner_message_callback.h"
+
+#define ROUTE_PLANNER_SERVICE_VERSION "Route Planner v1.0.0"
+
+
+#endif // ROUTE_PLANNER_H
